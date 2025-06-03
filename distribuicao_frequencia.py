eea65c56a986cd3b5e818d8a4749cb35f5790982
EFG <efg@efg.local>
2025-06-02 21:51:43 -0300

    aula02

--- distribuicao_frequencia.py
+++ distribuicao_frequencia.py
@@ -15,7 +15,39 @@ def analisar_dados_estatisticos(dados_brutos, nome_do_conjunto):
     print("\n2. Tamanho da Amostra (n):")
     print(f" n = {n}")
 
-# --- ESYTUDO DE CASO 01: IDADE DOS ALUNOS
+# 3. Valor Maximo e Mínimo
+    x_min = rol[0]
+    x_max = rol[-1]
+    print("\n3. Valor Máximo e Mínimo:")
+    print(f" valor min: {x_min}")
+    print(f" valor max: {x_max}")
+
+ # 4. Amplitude total (AT)
+    at = x_max - x_min   
+    print("\n4. Amplitude Total (AT): ")
+    print(f" AT = {at:.2f}")
+
+# 5. Número de classes (A)
+    k = math.ceil(math.sqrt(n))
+    print(f"\n5. Número de Classes (K):")
+    print(f"K = {k}")
+
+# 6. Amplitude das Classes (h)
+    h = at / k
+    print("\n6. Amplitude das Classes (h):")
+    print(f"h = {h}")
+    # Arredondar amplitude das Classes
+    num_casas_decimais = 0
+    if at < k: # 0.1
+        num_casas_decimais = 1
+        if at * 10 > k: #0.01
+            num_casas_decimais = 2
+    h = np.ceil(h * (10**num_casas_decimais)) / (10**num_casa_decimais)
+    print("\n6. Amplitude da Classes (h):")
+    print(f"h = {int(h)}")
+
+
+# --- ESTUDO DE CASO 01: IDADE DOS ALUNOS
 dados_idades = [21, 21, 21, 22, 22, 22, 23, 23, 23, 23, 24, 24, 25, 25, 25, 26, 27, 27, 28, 28, 30, 30, 31, 31, 31]
 df_idades = analisar_dados_estatisticos(dados_idades, "idades alunos")
 # print(df_idades.to_string())
