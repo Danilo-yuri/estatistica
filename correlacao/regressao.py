3fd5f9dfbc29170fafb622c47ed94352a9d7a2ca
EFG <efg@efg.local>
2025-08-11 21:25:08 -0300

    Grafico de regressao

--- correlacao/regressao.py
+++ correlacao/regressao.py
@@ -5,9 +5,38 @@ import seaborn as sns
 # pip install scikit-learn
 from sklearn.linear_model import LinearRegression
 
+# Carregamento e preparacao dos dados
+
 EXPOSICAO_ALGODAO = 'data/lungDisease.csv'
 dataframe = pd.read_csv(EXPOSICAO_ALGODAO)
 print(dataframe.head())
 
-dataframe.plot.scatter(x = 'Exposure', y = 'PEFR')
+# Grafico de DISPERÇÃO
+# dataframe.plot.scarret(x = 'Exposure', y = 'PEFR')
+# plt.show()
+
+#dataframe.plot.scatter(x = 'Exposure', y = 'PEFR')
+#plt.show()
+
+# 3. Configuração e treinamento do modelo
+# Defina a variavel preditora (independente), que e
+# a Exposure e a variavel de resultado que e o PEFR
+predictors= ['Exposure']
+outcome = 'PEFR'
+# Instanciar o modelo
+model = LinearRegression()
+# Treinar de treinamento
+model.fit(dataframe[predictors], dataframe[outcome])
+
+# 4. Exibição dos coeficiente
+# intercepto
+print(f'Intercepto: {model.intercept_:.3f}')
+# Coeficiente angular
+print(f'Coeficiente Angular: {model.coef_[0]}')
+# 5. Geração do grafico
+fig, (reg) = plt.subplots(1, 1, figsize=(4, 4))
+#grafico regreção
+
+reg = sns.regplot(x = 'Exposure', y = 'PEFR', data = dataframe,ax = reg)
+plt.tight_layout()
 plt.show()
