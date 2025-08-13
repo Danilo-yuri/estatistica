d723f45409dac2182de3d722cd6eb2474670794c
EFG <efg@efg.local>
2025-08-12 21:24:24 -0300

    Residuos

--- correlacao/regressao.py
+++ correlacao/regressao.py
@@ -34,9 +34,45 @@ print(f'Intercepto: {model.intercept_:.3f}')
 # Coeficiente angular
 print(f'Coeficiente Angular: {model.coef_[0]}')
 # 5. Geração do grafico
-fig, (reg) = plt.subplots(1, 1, figsize=(4, 4))
+fig, (reg, ax, res) = plt.subplots(1, 3, figsize=(12, 4))
 #grafico regreção
 
 reg = sns.regplot(x = 'Exposure', y = 'PEFR', data = dataframe,ax = reg)
+
+# Parcial
+# Definir os limites dos eixos x e y 
+ax.set_xlim(0, 23)
+ax.set_ylim(295, 450)
+# Definir os rotulos
+ax.set_xlabel('Exposure')
+ax.set_ylabel('PERF')
+#Plotar a linha
+ax.plot(dataframe['Exposure'], model.predict(dataframe[predictors]), '-')
+# Adicionar o texto b0
+ax.text(0.4, model.intercept_, r'$b_0$', size='larger')
+# Criar dataframe dos dados parciais e treinar
+x = pd.DataFrame({'Exposure': [7.5, 17.5]})
+y = model.predict(x)
+print(y)
+ax.plot((7.5, 7.5, 17.5), (y[0], y[1], y[1]), '--')
+# Exibir DeltaY e Deltax no grafico
+ax.text(5, np.mean(y), r'$\Delta Y$', size='larger')
+ax.text(12, y[1] - 10, r'$\Delta X$', size='larger')
+# Adicionar anotação para o coeficiente angular
+ax.text(12, 390, r'$b_1 = \frac{\Delta Y}{\Delta X}$', size='larger')
+
+#Valores ajustado e residuos
+#Gera os valores ajustados do modelo
+fitted = model.predict(dataframe[predictors])
+ #calcula os residuos
+residuals = dataframe[outcome] - fitted
+
+#Exibe o gráfico de correlação
+res = dataframe.plot.scatter(x = 'Exposure', y = 'PEFR', ax = res)
+res.plot(dataframe.Exposure, fitted)
+# Para cada valor de índece
+for x, yatual, yfitted in zip(dataframe.Exposure, dataframe.PEFR, fitted):
+    print(f'x: {x} - yreal: {yatual} - yreta: {yfitted}')
+    res.plot((x, x), (yatual,yfitted), '--', color='C1')
 plt.tight_layout()
 plt.show()
